2568f28eafc8c76598211676a7c82ca4fb42bd01
linw <linw@miligc.com>
2023-05-07 16:51:03 +0800

    add methods

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>首页</h1>
+    <button @click="handleClick">点击</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  methods:{
+    handleClick(){
+      console.log("-----")
+    }
+  }
+};
+</script>
